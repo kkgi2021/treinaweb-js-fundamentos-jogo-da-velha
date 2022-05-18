3398fcd92bbc49c7438f84e50cf0cc1b1a6276e6
claiton gimenis <cacagimenis@gmail.com>
2022-05-18 00:42:15 -0300

    adicionando linhas do jogo

--- src/index.js
+++ src/index.js
@@ -13,6 +13,11 @@ let jogo = {
   imprimir() {
     console.log(`
     ${jogo.campo[6]}|${jogo.campo[7]}|${jogo.campo[8]}
+    -----
+    ${jogo.campo[3]}|${jogo.campo[4]}|${jogo.campo[5]}
+    -----
+    ${jogo.campo[0]}|${jogo.campo[1]}|${jogo.campo[2]}
+    
     `);
   }
 };
