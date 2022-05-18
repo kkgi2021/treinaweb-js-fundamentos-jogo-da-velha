6dfa02ce64c7f0bd63d0db6252e4c3ba9c3195bb
claiton gimenis <cacagimenis@gmail.com>
2022-05-18 00:32:39 -0300

    criando a logica

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-
-`;
+let jogo = {
+  iniciarNovoJogo() {
+    jogo = {
+      ...jogo,
+      turno: 0,
+      jogadorAtual: "X",
+      campo: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      jogoFinalizado: false
+    };
+  },
+  imprimir() {
+    console.log(`
+    ${jogo.campo[6]}|${jogo.campo[7]}|${jogo.campo[8]}
+    `);
+  }
+};
+jogo.iniciarNovoJogo();
+jogo.imprimir();
